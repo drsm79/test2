28249b8acc3026281a20333027ac29203eea7e7f
Simon Metson <simon+github@cloudant.com>
2014-02-01 20:37:59 +0000

    try with carburetor

--- test2/foo.py
+++ test2/foo.py
@@ -1,5 +1,11 @@
 from test1.bar import bar
+from carburetor.word import generate
+
 
 def foo():
     print "foo fighters"
     bar()
+    print generate(10)
+
+if __name__ == '__main__':
+    foo()
